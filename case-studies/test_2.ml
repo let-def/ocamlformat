556ccecf64e49d9552405d4cc825ba29092430b2
Frédéric Bour <frederic.bour@lakaban.net>
2025-09-22 12:57:52 +0900

    Case study 2

---
+++ case-studies/test_2.ml
@@ -0,0 +1,13 @@
+let ( exception false ) @ lident = UIdent [@@ and ] let false = UIdent
+
+(* When ocamlformat processes this file, it produces:
+
+   ```
+   let exception false @ lident = UIdent [@@and]
+
+   let false = UIdent
+   ```
+
+   Unfortunately, for `@ lident` to parse the pattern has to be wrapped between
+   parentheses.
+*)
