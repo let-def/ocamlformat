41f5e5fd7b5de45280824a87051f597dbbd269a6
Frédéric Bour <frederic.bour@lakaban.net>
2025-09-22 11:51:03 +0900

    First case-study, trying to solve internal errors.

---
+++ case-studies/test_1.ml
@@ -0,0 +1,14 @@
+(*
+## Item `simple_expr: simple_expr . HASH LIDENT` (in 1651 errors)
+
+- Derivation (1651 occurrences):
+  ```
+  simple_expr: simple_expr . HASH LIDENT
+  ```
+  Sample sentence (implementation):
+  ```ocaml
+  lident # lident let false = UIdent
+  ```
+*)
+
+lident # lident let false = UIdent
